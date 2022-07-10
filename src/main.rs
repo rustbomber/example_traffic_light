9ba00e6d6fdd49f33598e3891405b3b68eaffd6d
rustbomber <ylh@wushikj.com>
2022-07-10 19:12:26 +0800

    add print

--- src/main.rs
+++ src/main.rs
@@ -21,12 +21,15 @@ fn main() {
     let yellow_light = TrafficLight::Yellow;
     let mut interval = yellow_light.get_interval();
     assert_eq!(interval, 5);
+    println!("yellow light interval: {}", interval);
 
     let red_light = TrafficLight::Red;
     interval = red_light.get_interval();
     assert_eq!(interval, 10);
+    println!("red light interval: {}", interval);
 
     let blue_light = TrafficLight::Blue;
     interval = blue_light.get_interval();
     assert_eq!(interval, 15);
+    println!("blue light interval: {}", interval);
 }
