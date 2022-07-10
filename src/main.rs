bf100aa5b6fb95164d6299ec94d94d8a990abaaa
rustbomber <ylh@wushikj.com>
2022-07-10 19:04:28 +0800

    init code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
-# example_traffic_light
-substrate入门第4课作业
+trait Light {
+    fn get_interval(&self) -> i32;
+}
+enum TrafficLight {
+    Red,
+    Blue,
+    Yellow,
+}
 
-为枚举交通信号灯实现一个 trait，trait里包含一个返回时间的方法，不同的灯持续的时间不同.
+impl Light for TrafficLight {
+    fn get_interval(&self) -> i32 {
+        match self {
+            TrafficLight::Red => 10,
+            TrafficLight::Blue => 15,
+            TrafficLight::Yellow => 5,
+        }
+    }
+}
 
-
-```rust
 fn main() {
     let yellow_light = TrafficLight::Yellow;
     let mut interval = yellow_light.get_interval();
@@ -18,4 +30,3 @@ fn main() {
     interval = blue_light.get_interval();
     assert_eq!(interval, 15);
 }
-```
